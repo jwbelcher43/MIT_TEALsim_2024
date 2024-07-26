a5a4ae86244eb8e587d28b5e907e80757a163582
jwbelcher43 <jbelcher@mit.edu>
2024-07-26 13:40:28 -0400

    adding release from latest build, no jar signing

--- MIT_TEALsim_2024/release/TEALsim-v0.4/java/src/teal/Version.java
+++ MIT_TEALsim_2024/release/TEALsim-v0.4/java/src/teal/Version.java
@@ -1,5 +1,5 @@
 /* Created by JReleaseInfo AntTask from Open Source Competence Group */
-/* Creation date Tue Jul 16 09:24:12 EDT 2024 */
+/* Creation date Fri Jul 26 09:47:56 EDT 2024 */
 package teal;
 
 import java.util.Date;
@@ -12,11 +12,11 @@ import java.util.Date;
 public class Version {
 
 
-   /** buildDate (set during build process to 1721136252301L). */
-   private static Date buildDate = new Date(1721136252301L);
+   /** buildDate (set during build process to 1722001676931L). */
+   private static Date buildDate = new Date(1722001676931L);
 
    /**
-    * Get buildDate (set during build process to Tue Jul 16 09:24:12 EDT 2024).
+    * Get buildDate (set during build process to Fri Jul 26 09:47:56 EDT 2024).
     * @return Date buildDate
     */
    public static final Date getBuildDate() { return buildDate; }
@@ -52,11 +52,11 @@ public class Version {
    public static final String getVersion() { return version; }
 
 
-   /** buildTimestamp (set during build process to "07/16/2024 09:24 AM"). */
-   private static String buildTimestamp = new String("07/16/2024 09:24 AM");
+   /** buildTimestamp (set during build process to "07/26/2024 09:47 AM"). */
+   private static String buildTimestamp = new String("07/26/2024 09:47 AM");
 
    /**
-    * Get buildTimestamp (set during build process to "07/16/2024 09:24 AM").
+    * Get buildTimestamp (set during build process to "07/26/2024 09:47 AM").
     * @return String buildTimestamp
     */
    public static final String getBuildTimestamp() { return buildTimestamp; }
